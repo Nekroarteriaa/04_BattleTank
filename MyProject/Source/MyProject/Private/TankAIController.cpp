ce850fd306151cc92d1a0c2c007eff3c492f7622
Olaf Gomez <Nekroarteriaa@gmail.com>
2019-02-06 11:59:59 +0100

    Creating AITankController Class

---
+++ MyProject/Source/MyProject/Private/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* ControlledTank = GetControlledTank();
+	UE_LOG(LogTemp, Warning, TEXT("AIControllerTank Begin Play"));
+
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Received tank's name is %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Nanai"));
+	}
+}
